fa69f328b807c64b1680b365c31275487b293726
devjin0617 <devjin0617@gmail.com>
2017-02-08 00:25:27 +0900

    #1: 설정파일들 수정진행

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, './public/dist'),
-        publicPath: '/public/dist/',
+        publicPath: '/dist/',
         filename: 'build.js'
     },
     module: {
