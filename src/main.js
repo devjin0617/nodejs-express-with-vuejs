0bc7087ecccb7d9df4e896e42a9255fef2158bf7
devjin0617 <devjin0617@gmail.com>
2017-02-08 01:03:53 +0900

    #2: ElementUI 적용 + 관련웹팩설정파일 수정

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import 'element-ui/lib/theme-default/index.css'
+
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App.vue'
+
+Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+});
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,22 @@ module.exports = {
                 options: {
                     name: '[name].[ext]?[hash]'
                 }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+                loader: 'file-loader',
+                query: {
+                name: '[name].[ext]?[hash]'
             }
+        }
         ]
     },
     resolve: {
