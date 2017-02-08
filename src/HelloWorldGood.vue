8dc0393ef7b5b5fa6e8933e677eb70e2e55e7f5c
devjin0617 <devjin0617@gmail.com>
2017-02-09 01:33:44 +0900

    #3: vuecon(vue-console) 개발진행 - 기본적인 vue파일 생성가능

---
+++ src/HelloWorldGood.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="hello-world-good">
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+/**import**/
+import TopBar from './components/TopBar.vue'
+import NaviBar from './components/NaviBar.vue'
+import Button from './components/Button.vue'
+
+export default {
+  name: 'hello-world-good',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App With Vuecon'
+    }
+  },
+  components: {
+    TopBar,
+		NaviBar,
+		Button
+  }
+}
+</script>
