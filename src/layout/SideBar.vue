e1132e7882aac3d1c121a5c9a97ecc024a7751f3
devjin0617 <devjin0617@gmail.com>
2017-02-09 00:17:58 +0900

    #2: Element 실험적으로 붙여보는 중

---
+++ src/layout/SideBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="side-bar">
+    <el-row class="tac">
+        <el-col :span="8">
+            <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+            <el-submenu index="1">
+                <template slot="title"><i class="el-icon-message"></i>Navigator One</template>
+                <el-menu-item-group title="Group One">
+                <el-menu-item index="1-1">item one</el-menu-item>
+                <el-menu-item index="1-2">item one</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group Two">
+                <el-menu-item index="1-3">item three</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                <template slot="title">item four</template>
+                <el-menu-item index="1-4-1">item one</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2"><i class="el-icon-menu"></i>Navigator Two</el-menu-item>
+            <el-menu-item index="3"><i class="el-icon-setting"></i>Navigator Three</el-menu-item>
+            </el-menu>
+        </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sideBar',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
---
+++ src/layout/TopBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="top-bar">
+        <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+            <el-menu-item index="1">Processing Center</el-menu-item>
+            <el-submenu index="2">
+                <template slot="title">Workspace</template>
+                <el-menu-item index="2-1">item one</el-menu-item>
+                <el-menu-item index="2-2">item two</el-menu-item>
+                <el-menu-item index="2-3">item three</el-menu-item>
+            </el-submenu>
+            <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'topBar',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
