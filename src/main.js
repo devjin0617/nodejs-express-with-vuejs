328eff597de33f65fe3c26a243ebb7333a10e9c9
devjin0617 <devjin0617@gmail.com>
2017-02-10 02:07:56 +0900

    #3: vuecon 설정에 따라 벤더라이브러리 추가여부 결정

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-import 'element-ui/lib/theme-default/index.css';
+import 'element-ui/lib/theme-default/index.css'
 
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-import App from './App.vue';
-Vue.use(ElementUI);
+import Vue from 'vue'
+import App from './App.vue'
+import VC from '../vuecon.config'
+
+if(VC.use.element) {  
+  Vue.use(require('element-ui'))
+}
+
+if(VC.use.jquery) {
+  window.$ = require('jquery')
+}
+
 
 new Vue({
   el: '#app',
