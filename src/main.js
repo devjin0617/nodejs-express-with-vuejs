39de7f31a869d598d244d085f4140163a7ab371c
devjin0617 <devjin0617@gmail.com>
2017-02-11 20:37:12 +0900

    #1: 컨벤션 변경에 따른 js파일 수정

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-default/index.css';
 
-import Vue from 'vue'
-import App from './App.vue'
-import VC from '../vuecon.config'
+import Vue from 'vue';
+import App from './App.vue';
+import VC from '../vuecon.config';
 
-if(VC.use.element) {  
-  Vue.use(require('element-ui'))
+if (VC.use.element) {
+  Vue.use(require('element-ui'));
 }
 
-if(VC.use.jquery) {
-  window.$ = require('jquery')
+if (VC.use.jquery) {
+  window.$ = require('jquery');
 }
 
 
